8433dc8cfc0751507e89a94d2374d0b419d7dae2
Grzegorz Grzybek <ggrzybek@redhat.com>
2021-03-10 09:49:10 +0100

    [tests] Fix integration tests after the merge

--- itests/test/src/test/java/org/apache/karaf/itests/features/EnterpriseFeaturesTest.java
+++ itests/test/src/test/java/org/apache/karaf/itests/features/EnterpriseFeaturesTest.java
@@ -55,11 +55,6 @@ public class EnterpriseFeaturesTest extends BaseTest {
     }
 
     @Test
-    public void installTransaction130Feature() throws Exception {
-        installAssertAndUninstallFeature("transaction", "1.3.3");
-    }
-
-    @Test
     public void installTransaction20Feature() throws Exception {
         installAssertAndUninstallFeature("transaction", "2.0.0");
     }
@@ -76,44 +71,8 @@ public class EnterpriseFeaturesTest extends BaseTest {
     }
 
     @Test
-    public void installOpenJpa222Feature() throws Exception {
-        installAssertAndUninstallFeature("openjpa", "2.2.2");
-    }
-
-    @Test
-    @RunIfNotOnJdk8
-    public void installOpenJpa230Feature() throws Exception {
-        installAssertAndUninstallFeature("openjpa", "2.3.0");
-    }
-
-    @Test
-    public void installOpenJpa240Feature() throws Exception {
-        installAssertAndUninstallFeature("openjpa", "2.4.2");
-    }
-
-    @Test
     public void installOpenJpa3Feature() throws Exception {
         installAssertAndUninstallFeatures("openjpa3");
-    }
-
-    @Test
-    public void installHibernate4215FinalFeature() throws Exception {
-        installAssertAndUninstallFeature("hibernate", "4.2.15.Final");
-    }
-
-    @Test
-    public void installHibernateEnvers4215FinalFeature() throws Exception {
-        installAssertAndUninstallFeature("hibernate-envers", "4.2.15.Final");
-    }
-
-    @Test
-    public void installHibernate436FinalFeature() throws Exception {
-        installAssertAndUninstallFeature("hibernate", "4.3.6.Final");
-    }
-
-    @Test
-    public void installHibernateEnvers436FinalFeature() throws Exception {
-        installAssertAndUninstallFeature("hibernate-envers", "4.3.6.Final");
     }
 
     @Test
